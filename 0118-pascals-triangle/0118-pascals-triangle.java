b2ca5da3d3842b9432112ed18abb842457361bc5
GAURAV SHARMA <70479366+theofficialgauravsharma@users.noreply.github.com>
2023-09-08 23:44:09 +0530

    Time: 1 ms (89.25%), Space: 40.6 MB (92.67%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,31 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        // Create an array list to store the output result...
-        List<List<Integer>> output = new ArrayList<List<Integer>>();
-        // Base cases...
-	    if (numRows <= 0)  return output;
-        // Create an array list to store the prev triangle value for further addition...
-	    ArrayList<Integer> prev = new ArrayList<Integer>();
-        // Inserting for the first row & store the prev array to the output array...
-	    prev.add(1);
-	    output.add(prev);
-        // For rest of the rows, Traverse all elements through a for loop...
-	    for (int i = 2; i <= numRows; i++) {
-            // Create another array to store the current triangle value...
-		    ArrayList<Integer> curr = new ArrayList<Integer>();
-		    curr.add(1);    //first
-            // Calculate for each of the next values...
-		    for (int j = 0; j < prev.size() - 1; j++) {
-                // Inserting the addition of the prev arry two values...
-			    curr.add(prev.get(j) + prev.get(j + 1));    //middle
-		    }
-            // Store the number 1...
-		    curr.add(1);    //last
-            // Store the value in the Output array...
-		    output.add(curr);
-            // Set prev is equal to curr...
-		    prev = curr;
-	    }
-	    return output;      // Return the output list of pascal values...
+        if (numRows == 0) return new ArrayList<>();
+        if (numRows == 1) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(Arrays.asList(1));
+            return result;
+        }
+        
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+        
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+        
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+        
+        prevRows.add(newRow);
+        return prevRows;
     }
 }
